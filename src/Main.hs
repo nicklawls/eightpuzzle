fee8bea783a2292cb45c77d431c13f46b06b3b15
Nick Lawler <lawler145@gmail.com>
2015-10-30 17:00:06 -0700

    refactor type variables, reached pretty looking but broken interface

--- src/Main.hs
+++ src/Main.hs
@@ -6,69 +6,74 @@ import Data.List (sortBy, foldl')
 import Data.Ord (comparing)
 import Data.PQueue.Prio.Min (MinPQueue, singleton, minView, insert)
 
-{- We'll start by defining a few abstract building blocks for pieces of the general
-   search algorithm. Allowing us to generalize to any NPuzzle, or any other
-   search problem, as long as we can implement this interface
+{-  We'll start by defining a few abstract building blocks for pieces of the general
+    search algorithm. Allowing us to generalize to any NPuzzle, or any other
+    search problem, as long as we can implement this interface
 
-   We'll then define concrete datatypes for the eight puzzle problem
-   and connect them to the abstract interface.
+    We'll then define concrete datatypes for the eight puzzle problem
+    and connect them to the abstract interface.
 -}
 
-{- Since the state of the problem can in principle be any datatype, we'll
-   represent it with a a type variable ;a'. The keyword 'type' denotes a type
-   synonym, letting us use 'State a' in place of a plain 'a', making our code
-   follow the languge of the problem a bit more closely
+{-  In order to provide a generic interface for other search problem, we'll be
+    making use of Haskell's hefty type system. See the follwoing link if you get
+    confused: http://joelburget.com/data-newtype-instance-class/
 -}
 
-type State a = a
 
-{- an Operator for type 'a' is simply a function from 'State a' to a list of 'State a's
-   i.e. from a current state to a set of new states
+{-  An Operator for type 'state' is simply a function from a 'state' to a list of 'states's
+    i.e. from a current state to a set of child states. Each operator also has
+    an associated cost which we can represent as simply an integer
 -}
 
--- TODO Tuple up the Cost = Int with the operator function
--- Make sure all operators return the empty list when they're undefined
-type Operator a =  State a -> [State a]
 
-{- this is a typeclass declaration, basically saying that a type 'a' represets
-   a Problem if it is an instance of the 'Ord' class, and if the two functions
-   'expand' and 'isGoal' and the value 'initialState' are defined. We will
-   define these functions for EightPuzzle in an 'instance' declaration later on.
+type Cost = Int -- 'Cost' is now an alias for the primitive type 'Int'
+
+type Operator state = ( Cost, state -> [state] )
+-- 'state' is a type variable that any type can inhabit
+-- 'a -> b' defines a function from a to b
+-- (a,b) is a tuple with inhabitants of possibly different type
+
+{- Below is a typeclass declaration, basically saying that a type 'a' represets
+   a 'Problem' if it has a set of 'Operators', and if the two functions
+   'expand' and 'isGoal' are defined. We will define these functions for
+   'EightPuzzle' in an 'instance' declaration later on.
 
    Note that even with this highly general definition, we can already implement
-   expand for any possible 'a'! Simply apply all the operators to the state,
+   expand for any possible 'state'! Simply apply all the operators to the state,
    and stitch together the resultant lists of states. Haskell's typeclass
    syntax allows us to a provide a default implementation in the declaration.
 -}
 
-class Problem a where
-    expand :: State a -> [State a]
-    isGoal :: State a -> Bool
-    operators :: [Operator a]
+class Problem state where
+    isGoal :: state -> Bool
+    operators :: [Operator state]
+    expand :: state -> [state]
     expand state =
-        concat (applyAll operators state)
+        concat (applyAll (map snd operators) state)
         -- reads: "concatinate the results of applying all operators to the state "
 
 -- apply each function in a list of functions to some value, returning a list of results
--- TODO implement this with a fold
 applyAll :: [a -> b] -> a -> [b]
 applyAll functions state =
     case functions of
         [] -> []
         (firstFunction:rest) -> firstFunction state : applyAll rest state
-
+-- 'state' here is a value-level function argument, not a type variable
 
 
 {-  The queueing function takes the current queue and the expanded states, and
     inserts them into the queue in some order. The order of insertion determines
     the nature of the search algorithm. For our class of problems, we'll use a
-    min priority queue.
+    min priority queue that will dequeue its entries in order of increasing 'Cost'.
+
 -}
 
-type Queue k a = MinPQueue k a
-type QueueingFunction k a = Queue k (State a) -> [State a] -> Queue k (State a)
+type Queue state = MinPQueue Cost state
+-- forces us to use 'Int' valued Cost for all problems
 
-{- We can finally write the general search algorithm, which takes any 'State a'
+type QueueingFunction state = Queue state -> [state] -> Queue state
+
+{- We can finally write the general search algorithm, which takes anything of type 'state'
    implementing the 'Problem' interface, plus a queueing function with which to
    order the insertion into the search queue.
 
@@ -82,9 +87,9 @@ type QueueingFunction k a = Queue k (State a) -> [State a] -> Queue k (State a)
    function also uses the 'do' syntax sugar to look more imperative
 -}
 
-generalSearch :: (Problem a, Ord k) => k -> State a -> QueueingFunction k a -> Either String (State a)
-generalSearch score state =
-    go (singleton score state)
+generalSearch :: Problem state => state -> QueueingFunction state -> Either String state
+generalSearch state =
+    go (singleton 0 state) -- start with initial cost 0
         where go nodes qf = do
                 (node, queue) <- case minView nodes of
                                     Nothing -> Left "No Solution"
@@ -104,23 +109,23 @@ generalSearch score state =
 -}
 
 -- TODO fix when cost is tupled
-astar :: Problem a => (State a -> Int) -- g(x)
-                   -> (State a -> Int) -- h(x)
-                   -> Queue Int (State a) -- Initial queue
-                   -> [State a] -- expanded nodes
-                   -> Queue Int (State a) -- New Queue
+astar :: Problem state => (state -> Cost) -- the function g(x)
+                       -> (state -> Cost) -- the function h(x)
+                       -> Queue state     -- initial queue
+                       -> [state]         -- expanded nodes
+                       -> Queue state     -- new queue
 astar g h queue nodes =
     let f x = g x + h x
     in  insertAll queue (map (\node -> (f node, node)) nodes)
 
 
-insertAll :: (Ord k, Problem a) => Queue k (State a) -> [(k,State a)] -> Queue k (State a)
+insertAll :: Problem state => Queue state -> [(Cost,state)] -> Queue state
 insertAll = foldl' (flip (uncurry insert))
 -- this magical incantation inserts the list of (score, node) pairs into the queue
 
 
 -- TODO Fix this when cost is tupled
-uniformCost :: Problem a => QueueingFunction Int a
+uniformCost :: Problem state => QueueingFunction state
 uniformCost = astar (const 1) (const 0) -- 'const 0' is a function that always returns 0
 
 {- Now that we have a general framework set up, lets define the Eight Puzzle
